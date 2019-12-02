0cd5dca5912cb0ab674444b75401952c5c178ab7
Gregory <Gregory@gregory.nl>
2019-12-02 10:40:08 +0100

    Day2 part1 done

--- Day1.php
+++ Day1.php
@@ -1,7 +1,7 @@
 <?php
 //Day 1 Part 1
 echo "Part 1: <br>";
-$input = explode("\n", file_get_contents('input.txt'));
+$input = explode("\n", file_get_contents('input1.txt'));
 $totalFuel = 0;
 foreach($input as $mass){
     $fuel = floor((int) $mass / 3) - 2;
@@ -11,7 +11,7 @@ echo $totalFuel;
 
 //Day 1 Part 2
 echo "<br>Part 2: <br>";
-$input = explode("\n", file_get_contents('input.txt'));
+$input = explode("\n", file_get_contents('input1.txt'));
 $totalFuel = 0;
 foreach($input as $mass){
     $fuel = floor((int) $mass / 3) - 2;
