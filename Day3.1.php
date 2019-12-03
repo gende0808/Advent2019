8a944f420950a7739c9115373e7c65cc50b827ab
Gregory <Gregory@gregory.nl>
2019-12-03 16:32:44 +0100

    Day 3 Part 1 done

--- Day3.1.php
+++ Day3.1.php
@@ -1,29 +1,89 @@
 <?php
-
+error_reporting(0);
 $lines = explode("\n", file_get_contents("Day3.txt"));
-
 $line1 = explode(",", $lines[0]);
 $line2 = explode(",", $lines[1]);
-$x = 0;
-$y = 0;
 $grid = array(array());
+$overlaps = array();
+$result1 = fill_array($grid, $line1, false);
+$result2 = fill_array($result1, $line2, true);
+unset($overlaps[0]);
+print_array(min($overlaps));
 
-foreach ($line1 as $movement) {
-    $direction = $movement[0];
-    $distance = substr($movement, 1);
-    if ($direction == "U") {
-        $y = $distance;
-        for ($dist = 0; $dist < $distance; $dist++) {
-            $grid[$x][$dist] = "X";
+function fill_array($grid, $line, $special)
+{
+    $x = 0;
+    $y = 0;
+    global $overlaps;
+    foreach ($line as $movement) {
+        $direction = $movement[0];
+        $distance = substr($movement, 1);
+        if ($direction == "U") {
+            $resultDistance = $y + $distance;
+            while ($y < $resultDistance) {
+                if ($special == true) {
+                    if($grid[$y][$x] == "X" || $grid[$y][$x] == "!"){
+                        $grid[$y][$x] = "!";
+                        $overlaps[abs($y) + abs($x)] = abs($y) + abs($x);
+                    }else{
+                        $grid[$y][$x] = "O";
+                    }
+                } else{
+                    $grid[$y][$x] = "X";
+                }
+                $y++;
+            }
+        }
+        if ($direction == "D") {
+            $resultDistance = $y - $distance;
+            while ($y > $resultDistance) {
+                if ($special == true) {
+                    if($grid[$y][$x] == "X" || $grid[$y][$x] == "!"){
+                        $grid[$y][$x] = "!";
+                        $overlaps[abs($y) + abs($x)] = abs($y) + abs($x);
+                    }else{
+                        $grid[$y][$x] = "O";
+                    }
+                } else{
+                    $grid[$y][$x] = "X";
+                }
+                $y--;
+            }
+        }
+        if ($direction == "R") {
+            $resultDistance = $x + $distance;
+            while ($x < $resultDistance) {
+                if ($special == true) {
+                    if($grid[$y][$x] == "X" || $grid[$y][$x] == "!"){
+                        $grid[$y][$x] = "!";
+                        $overlaps[abs($y) + abs($x)] = abs($y) + abs($x);
+                    }else{
+                        $grid[$y][$x] = "O";
+                    }
+                } else{
+                    $grid[$y][$x] = "X";
+                }
+                $x++;
+            }
+        }
+        if ($direction == "L") {
+            $resultDistance = $x - $distance;
+            while ($x > $resultDistance) {
+                if ($special == true) {
+                    if($grid[$y][$x] == "X" || $grid[$y][$x] == "!"){
+                        $grid[$y][$x] = "!";
+                        $overlaps[abs($y) + abs($x)] = abs($y) + abs($x);
+                    }else{
+                        $grid[$y][$x] = "O";
+                    }
+                } else{
+                    $grid[$y][$x] = "X";
+                }
+                $x--;
+            }
         }
     }
-    if ($direction == "D") {
-        $y = $distance;
-        for ($dist = 0; $dist < $distance; $dist++) {
-            $grid[$x][$dist] = "X";
-        }
-    }
-
+    return $grid;
 
 }
 
