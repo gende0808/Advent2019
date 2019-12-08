755d55abd5c84de5ffc5d24775e4e9fd21bc77b5
Gregory <gende@novacollege.nl>
2019-12-08 23:52:08 +0100

    Day 8 part 2 done.

--- Day8.2.php
+++ Day8.2.php
@@ -1,14 +1,44 @@
+<style>
+    div{
+        height: 10px;
+        width: 10px;
+        margin: 0;
+    }
+    .white{
+        float:left;
+        background-color: white;
+    }
+    .black{
+        float:left;
+        background-color: black;
+    }
+    .breaker{
+        margin-right: auto;
+    }
+</style>
 <?php
+include "functions.php";
+
 $complete_array = str_split(file_get_contents("Day8.txt"), 25 * 6);
 
-$lowest_count = 200;
-$answer = 0;
-foreach ($complete_array as $key => $row) {
-    $occurrences = count_chars($row, 1);
-    $number_of_zeros = $occurrences[48];
-    if ($number_of_zeros < $lowest_count) {
-        $lowest_count = $number_of_zeros;
-        $answer = $occurrences[49] * $occurrences[50];
+$current_row = str_split($complete_array[0],1);
+foreach($complete_array as $row){
+    $row = str_split($row, 1);
+    foreach($row as $key => $number){
+        if($current_row[$key] >= 2){
+            $current_row[$key] = $number;
+        }
     }
 }
-echo $answer;
+foreach($current_row as $key => $number){
+    if($key % 25 == 0){
+        echo "<div class='breaker'></div>";
+    }
+    if($number == 1){
+        echo "<div class='white'></div>";
+    }
+    if($number == 0){
+        echo "<div class='black'></div>";
+    }
+}
+?>
