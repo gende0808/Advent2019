1dd005ec93be09a8717fa19320d54e5b8bcc6b24
gregory van den Ende <gende@novacollege.nl>
2019-12-18 16:42:13 +0100

    bla

--- Day6.1.php
+++ Day6.1.php
@@ -6,11 +6,17 @@ $indexed_array = array();
 $map_of_planets = array();
 foreach ($lines as $line) {
     list($key, $value) = explode(')', $line);
-    $indexed_array[$key] = $value;
+    $indexed_array[$key] = (string) trim($value, "\ \t\n\r\0\x0B");
 }
-$amount = 0;
-$pointer = [1,"WLQ"];
-while(array_search($pointer[1], $indexed_array)){
-    $amount = $pointer[0] + 1;
-    $pointer
+$total_count = 0;
+print_array($indexed_array);
+foreach ($indexed_array as $key => $value) {
+    $count = 0;
+    $thing = $value;
+        while($thing != "COM"){
+        $new_key = array_search($thing, $indexed_array);
+        $thing = $indexed_array[$new_key];
+        $count++;
+    }
+    $total_count += $count;
 }
